4ca8bb946b7d78807218ac8992dad22f2e817fcf
Marimar03 <md.acuna@alumno.um.edu.ar>
2023-04-11 12:16:13 -0300

    subo archivos

---
+++ test_palindromo.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class TestPalindrome(unittest.TestCase):
+    def test_palindrome_simple(self):
+        result = palindrome('neuquen')
+        self.assertEqual(result, True)
+
+if __name__ == '__main__':
+    unittest.main()
